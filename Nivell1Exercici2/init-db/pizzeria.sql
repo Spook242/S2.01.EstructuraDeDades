932562b6c096ea1de68dde7a388add0603b2bf4b
Spook242 <spook242@gmail.com>
2025-11-01 19:27:01 +0100

    Initial commit

---
+++ Nivell1Exercici2/init-db/pizzeria.sql
@@ -0,0 +1,5 @@
+INSERT INTO store (address, zip_code, town_id) VALUES
+('Carrer Gran de Gracia, 123', '08012', 1);
+
+INSERT INTO store (address, zip_code, town_id) VALUES
+('Avinguda del Paisos Catalans, 45', '43205', 5);
