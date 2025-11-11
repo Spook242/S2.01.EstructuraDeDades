aa6ac635ad295b338e41bb8ed2d246dc418d1536
Spook242 <spook242@gmail.com>
2025-11-11 10:50:38 +0100

    Delete pizzeria.sql

--- Nivell1Exercici2/init-db/pizzeria.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO store (address, zip_code, town_id) VALUES
-('Carrer Gran de Gracia, 123', '08012', 1);
-
-INSERT INTO store (address, zip_code, town_id) VALUES
-('Avinguda del Paisos Catalans, 45', '43205', 5);
